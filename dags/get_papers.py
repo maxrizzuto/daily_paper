2746e703a2480a081e53d70bd43487db071071bb
Max Rizzuto <mrizzuto@proton.me>
2024-11-26 00:16:15 -0500

    Added database_readings task, removed example DAGs

--- dags/get_papers.py
+++ dags/get_papers.py
@@ -1,6 +1,11 @@
+import json
+from datetime import datetime, timedelta
+
 import requests
+from airflow import DAG
 from airflow.decorators import dag, task
 from airflow.models import Variable
+from lxml import html
 
 with DAG(
     dag_id="get_papers",
@@ -13,12 +18,42 @@ with DAG(
         "retry_delay": timedelta(minutes=2)
     }
 ) as dag:
+    
     @task
-    def scholar_to_s3():
+    def database_readings():
+        """
+        Make sure to reference source name and url when storing to S3 for attribution purposes
+        """
+
+        # construct tree for scraping from website
+        src = 'https://github.com/rxin/db-readings'
+        page = requests.get(src)
+        tree = html.fromstring(page.content)
+
+        # get papers and add to dictionary
+        papers = dict()
+        subcategories = tree.xpath('//ul/preceding-sibling::div[@class="markdown-heading"]')[2:-2]
+        for subcategory in subcategories:
+            subcat_name = subcategory.xpath('./h2/a')[0].text.strip()
+            subcat_papers = subcategory.xpath('./following-sibling::ul[1]//a[contains(@href, ".pdf")]')
+            for paper in subcat_papers:
+                paper_link = src + paper.xpath('./@href')[0]
+                paper_name = paper.text
+                papers[paper_link] = {'name': paper_name, 'category': 'databases', 'subcategory': subcat_name}
+
+        print(json.dumps(papers, sort_keys=True, indent=4))
+        return papers
+
+    @task
+    def data_source_b():
+        pass
+
+    @task
+    def data_source_c():
         pass
 
     @task
     def s3_to_rds():
         pass
 
-    scholar_to_s3 >> s3_to_rds
+    [database_readings(), data_source_b(), data_source_c()] >> s3_to_rds()
